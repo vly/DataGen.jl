743a94603ff88250771ddc7ab336ea64f240abdf
Val Lyashov <val@plstr.com>
2021-03-04 11:12:07 +1100

    scaffolding for new iteration

---
+++ src/archetype.jl
@@ -0,0 +1,18 @@
+# set of predefined use-case archetype
+
+# interaction events
+function gen_traffic() end
+
+# transactional data
+function gen_sales() end
+function gen_subscriptions() end
+function gen_invoices() end
+
+# customers
+function gen_users() end
+function gen_subscribers() end
+
+# products
+function gen_products() end
+function gen_categories() end
+function gen_subscription_plans() end
